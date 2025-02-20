4a80297e417bb81276d224c9046f304a2a168a53
Nimira43 <n.rabone72@aol.com>
2025-02-20 23:30:05 +0000

    Copied exsiting FormField - this new FormField will be altered to display the CategoryID

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -51,6 +51,29 @@ export function TransactionForm() {
               )
             }}
           />
+          <FormField
+            control={form.control} name='transactionType'
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Transaction Type</FormLabel>
+                  <FormControl>
+                    <Select
+                      value={field.value} onValueChange={field.onChange}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder='Transaction Type'/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value='income'>Income</SelectItem>                   <SelectItem value='expense'>Expense</SelectItem>    
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )
+            }}
+          />
         </fieldset>
       </form>  
     </Form>
