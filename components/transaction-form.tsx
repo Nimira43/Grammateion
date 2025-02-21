73245cc35713e8cb65e45f4e9f03df6530457b33
Nimira43 <n.rabone72@aol.com>
2025-02-21 00:29:43 +0000

    Corrected typo - added [ ] to pixel value on line 99.

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -96,7 +96,7 @@ export function TransactionForm() {
                         <Button
                           variant={'outline'}
                           className={cn(
-                            'w-280px justify-center text-left font-normal',
+                            'w-[280px] justify-center text-left font-normal',
                             !field.value && 'text-muted-foreground'
                           )}
                         >
