0825590c37d382ea8efe01b5d2981e24a20087c0
Nimira43 <n.rabone72@aol.com>
2025-02-21 00:18:55 +0000

    Setup Popover and Calendar components and used field.value and field.onChange where needed.

--- components/transaction-form.tsx
+++ components/transaction-form.tsx
@@ -1,13 +1,14 @@
 import { useForm } from 'react-hook-form'
-import { date, z } from 'zod'
+import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
-import { Popover, PopoverTrigger } from './ui/popover'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 import { Button } from './ui/button'
 import { cn } from '@/lib/utils'
 import { CalendarIcon } from 'lucide-react'
 import { format } from 'date-fns'
+import { Calendar } from './ui/calendar'
 
 const transactionFormSchema = z.object({
   transactionType: z.enum(['income', 'expense']),
@@ -95,17 +96,25 @@ export function TransactionForm() {
                           variant={'outline'}
                           className={cn(
                             'w-280px justify-center text-left font-normal',
-                            !date && 'text-muted-foreground'
+                            !field.value && 'text-muted-foreground'
                           )}
                         >
                           <CalendarIcon className='mr-2 h-4 w-4' />
-                          {date ? (
-                            format(date, 'PPP')
+                          {field.value ? (
+                            format(field.value, 'PPP')
                           ) : (
                             <span>Pick Date</span>
                           )}
                         </Button>
                       </PopoverTrigger>
+                      <PopoverContent className='w-auto p-0'>
+                        <Calendar
+                          mode='single'
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                        />
+                      </PopoverContent>
                     </Popover>
                   </FormControl>
                   <FormMessage />
