2da418a8c68825e599c0c2124854a31b8f8b453d
Nimira43 <n.rabone72@aol.com>
2025-04-03 20:35:45 +0100

    Completed fields for categoriesTable.

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,9 @@
-import { pgTable } from 'drizzle-orm/pg-core'
+import { integer, pgTable, text } from 'drizzle-orm/pg-core'
 
 export const categoriesTable = pgTable('categories', {
-  
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  type: text({
+    enum: ['income', 'expense']
+  })
 })
