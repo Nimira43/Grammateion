dbaf871eee75d3fa8a4cde6cdb786954eeaf2a31
Nimira43 <n.rabone72@aol.com>
2025-05-04 22:40:56 +0100

    Setting up a server function using createServerFn from @tanstack/start to fetch categories from database

---
+++ data/getCategories.ts
@@ -0,0 +1,9 @@
+import { db } from '@/db'
+import { categoriesTable } from '@/db/schema'
+import { createServerFn } from '@tanstack/start'
+
+export const getCategories = createServerFn({
+  method: 'GET'
+}).handler(async () => {
+  const categories = await db.select().from(categoriesTable)
+})
