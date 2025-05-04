c6c68a9dc4f48d171a819e88afb0be267df88550
Nimira43 <n.rabone72@aol.com>
2025-05-04 22:51:35 +0100

    Updating _layout.index.tsx

--- app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
+++ app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
@@ -1,21 +1,30 @@
 import { TransactionForm } from '@/components/transaction-form'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getCategories } from '@/data/getCategories'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
   '/_authed/dashboard/transactions/new/_layout/',
 )({
   component: RouteComponent,
+  loader: async () => {
+    const categories = await getCategories()
+    return {
+      categories
+    }
+  }
 })
 
 function RouteComponent() {
+  const { categories } = Route.useLoaderData()
+
   return (
     <Card className='max-w-screen-md mt-4'>
       <CardHeader>
         <CardTitle>New Transaction</CardTitle>
       </CardHeader>
       <CardContent>
-        <TransactionForm />
+        <TransactionForm categories={categories} />
       </CardContent>
     </Card>
   )
