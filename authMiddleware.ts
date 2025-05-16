308c1466f665a8878bd9712d6ad9a21d20d68558
Nimira43 <n.rabone72@aol.com>
2025-05-16 21:10:04 +0100

    Importing getAuth and getWebRequest to help set up authMiddleware

--- authMiddleware.ts
+++ authMiddleware.ts
@@ -1,3 +1,8 @@
+import { getAuth } from '@clerk/tanstack-start/server'
 import { createMiddleware } from '@tanstack/start'
+import { getWebRequest } from 'vinxi/http'
 
-const authMiddleware = createMiddleware
+const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const user = await getAuth(getWebRequest())
+
+})
