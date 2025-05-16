221f5961cf4413123688f4de363592b1bf4b6cab
Nimira43 <n.rabone72@aol.com>
2025-05-16 20:38:54 +0100

    Copied in transactionFormSchema into createTransaction and starting to adapt it for use here.

--- data/createTransaction.ts
+++ data/createTransaction.ts
@@ -1,5 +1,25 @@
 import { createServerFn } from '@tanstack/start'
 
+const transactionSchema = z.object({
+  transactionType: z
+    .enum(['income', 'expense']),
+  categoryId: z.coerce
+    .number()
+    .positive('Please select a category.'),
+  transactionDate: z
+    .date()
+    .max(addDays(new Date(), 1), 'Transaction date cannot be a future date.'),
+  amount: z.coerce
+    .number()
+    .positive('Amount must be a positive number.'),
+  description: z
+    .string()
+    .min(5, 'Description must contain at least 5 characters.')
+    .max(300, 'Description must not exceed 300 characters.')
+})
+
 export const createTransaction = createServerFn({
   method: 'POST'
+}).validator(() => {
+
 })
