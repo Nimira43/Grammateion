93a38ef786269e99027ffae10bcf3e8aa2db44e9
Nimira43 <n.rabone72@aol.com>
2025-06-04 05:51:02 +0100

    Created searchSchema variable

--- app/routes/_authed/dashboard/transactions/_layout.index.tsx
+++ app/routes/_authed/dashboard/transactions/_layout.index.tsx
@@ -1,9 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { z } from 'zod'
+
+const today = new Date()
+const searchSchema = z.object({
+  month: z.number().min(1).max(12).optional(),
+  year: z.number().min(today.getFullYear() - 100).max(today.getFullYear()).optional()
+})
 
 export const Route = createFileRoute(
   '/_authed/dashboard/transactions/_layout/',
 )({
   component: RouteComponent,
+  validateSearch:
 })
 
 function RouteComponent() {
