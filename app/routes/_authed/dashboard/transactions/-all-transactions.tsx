d8e06bdde2a994deb9804e906498b30e7cc48881
Nimira43 <n.rabone72@aol.com>
2025-06-21 02:34:47 +0100

    Passing in props and types

--- app/routes/_authed/dashboard/transactions/-all-transactions.tsx
+++ app/routes/_authed/dashboard/transactions/-all-transactions.tsx
@@ -1,11 +1,17 @@
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
-export function AllTransactions() {
+export function AllTransactions({
+  month,
+  year
+}: {
+  month: number
+  year: number
+}) {
   return (
     <Card>
       <CardHeader>
         <CardTitle>
-          
+
         </CardTitle>
       </CardHeader>
     </Card>
