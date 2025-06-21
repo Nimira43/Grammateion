1871b7090718dfd6b1ae9ddea4ba293daa0dc510
Nimira43 <n.rabone72@aol.com>
2025-06-21 03:14:13 +0100

    Working on the month select options

--- app/routes/_authed/dashboard/transactions/-all-transactions.tsx
+++ app/routes/_authed/dashboard/transactions/-all-transactions.tsx
@@ -1,4 +1,6 @@
 import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/ui/select'
+import { SelectValue } from '@radix-ui/react-select'
 import { format } from 'date-fns'
 
 export function AllTransactions({
@@ -19,7 +21,21 @@ export function AllTransactions({
           <span className='font-medium'>
             Transactions for {format(selectedDate, 'MMMM yyyy')} 
           </span>
-          <div>Select</div>
+          <div className='flex gap-1'>
+            <Select>
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                {Array.from({length: 12}).map((_, i) => (
+                  <SelectItem value={`${i + 1}`}>
+                    {format(new Date(selectedDate.getFullYear(), i, 1), 'MMMM')}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+
+          </div>
           
         </CardTitle>
       </CardHeader>
