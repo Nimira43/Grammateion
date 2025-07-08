e111b78f4f499b4cf84c9572f279bbb32aabd3ec
Nimira43 <n.rabone72@aol.com>
2025-07-08 02:47:16 +0100

    Staarting to create the schema

--- data/getTransactionsByMonth.tsx
+++ data/getTransactionsByMonth.tsx
@@ -1,7 +1,11 @@
 import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
 
-
+const schema = z.object({
+  month: z.number(),
+  years: z.number()
+})
 
 
 export const getTransactionsByMonth = createServerFn({
