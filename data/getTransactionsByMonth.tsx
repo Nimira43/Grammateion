4da270baad23a0a26fd7453da122ac125d29025e
Nimira43 <n.rabone72@aol.com>
2025-07-08 02:51:58 +0100

    Passing schema into getTransactionsByMonth

--- data/getTransactionsByMonth.tsx
+++ data/getTransactionsByMonth.tsx
@@ -2,12 +2,21 @@ import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
 import { z } from 'zod'
 
-const schema = z.object({
-  month: z.number(),
-  years: z.number()
-})
+const today = new Date()
 
+const schema = z.object({
+  month: z
+    .number()
+    .min(1)
+    .max(12),
+  years: z
+    .number()
+    .min(today.getFullYear() - 100)
+    .max(today.getFullYear())
+})
 
 export const getTransactionsByMonth = createServerFn({
   method: 'GET'
-}).middleware([authMiddleware]).validator()
+}).middleware([authMiddleware]).validator((data: z.infer<typeof schema>) => {
+  schema.parse(data)
+})
