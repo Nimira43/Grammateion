2edceecf83745e5ea4bc5d627c7c0214b4df2292
Nimira43 <n.rabone72@aol.com>
2025-08-08 02:15:34 +0100

    handleSubmit and categories

--- app/routes/_authed/dashboard/transactions/$transactionid/_layout.index.tsx
+++ app/routes/_authed/dashboard/transactions/$transactionid/_layout.index.tsx
@@ -1,21 +1,34 @@
 import { TransactionForm } from '@/components/transaction-form'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { getCategories } from '@/data/getCategories'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
   '/_authed/dashboard/transactions/$transactionid/_layout/',
 )({
   component: RouteComponent,
+  loader: async () => {
+    const categories = await getCategories()
+    return {
+      categories
+    }
+  }
 })
 
 function RouteComponent() {
+  const { categories } = Route.useLoaderData() 
+  const handleSubmit = async () => {}
+
   return (
     <Card className='max-w-screen-md mt-4'>
       <CardHeader>
         <CardTitle>Edit Transaction</CardTitle>
       </CardHeader>
       <CardContent>
-        Test
+        <TransactionForm
+          categories={categories}
+          onSubmit={handleSubmit}
+        />
       </CardContent>
     </Card>
   )
