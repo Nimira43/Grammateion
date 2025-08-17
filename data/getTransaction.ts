cb81568e5e374fcabc980c337896c9d974e0569c
Nimira43 <n.rabone72@aol.com>
2025-08-17 15:40:20 +0100

    Created schema and passed into validator

--- data/getTransaction.ts
+++ data/getTransaction.ts
@@ -1,6 +1,11 @@
 import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
+
+const schema = z.object({
+  transactionId: z.number()
+})
 
 export const getTransaction = createServerFn({
   method: 'GET',
-}).middleware([authMiddleware]).validator()
+}).middleware([authMiddleware]).validator(schema)
