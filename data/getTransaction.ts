787005cbe5e4500b1d284679c379f7cfbe5e5441
Nimira43 <n.rabone72@aol.com>
2025-08-17 15:39:04 +0100

    authMiddleware

--- data/getTransaction.ts
+++ data/getTransaction.ts
@@ -1,5 +1,6 @@
+import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
 
 export const getTransaction = createServerFn({
   method: 'GET',
-})
+}).middleware([authMiddleware]).validator()
