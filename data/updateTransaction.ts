1ccd4c51e9d87526315e9d358630e8af9693b40f
Nimira43 <n.rabone72@aol.com>
2025-08-26 04:11:08 +0100

    Creating updateTransaction.ts - 4

--- data/updateTransaction.ts
+++ data/updateTransaction.ts
@@ -1,6 +1,25 @@
 import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
+
+const schema = z.object({
+  id: z.number(),
+  categoryId: z.coerce
+      .number()
+      .positive('Please select a category.'),
+    transactionDate: z.string().refine((value) => {
+      const parsedDate = new Date(value)
+      return !isNaN(parsedDate.getTime()) && parsedDate <= addDays(new Date(), 1)
+    }),
+    amount: z.coerce
+      .number()
+      .positive('Amount must be a positive number.'),
+    description: z
+      .string()
+      .min(5, 'Description must contain at least 5 characters.')
+      .max(300, 'Description must not exceed 300 characters.')
+})
 
 export const updateTransaction = createServerFn({
   method: 'POST'
-}).middleware(authMiddleware).validator()
+}).middleware([authMiddleware]).validator()
