7a6f3183f2c97af8f8cb0f1aa7142da3f969d08e
Nimira43 <n.rabone72@aol.com>
2025-09-08 21:03:12 +0100

    deleteTransaction - 3

--- data/deleteTransaction.ts
+++ data/deleteTransaction.ts
@@ -1,5 +1,9 @@
 import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
+
+const schema = z.object({
+  transactionId: z.number()})
 
 export const deleteTransaction = createServerFn({
   method: 'POST'
