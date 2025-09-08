c3014167436c2805f47d2af55fae88c7efbf5975
Nimira43 <n.rabone72@aol.com>
2025-09-08 20:54:19 +0100

    deleteTransaction - 2

--- data/deleteTransaction.ts
+++ data/deleteTransaction.ts
@@ -1,3 +1,6 @@
+import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
 
-export const deleteTransaction = createServerFn({})
+export const deleteTransaction = createServerFn({
+  method: 'POST'
+}).middleware([authMiddleware]).validator()
