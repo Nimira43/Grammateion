dcf214d5b4617d016a7738c29a91e95da3c72ce8
Nimira43 <n.rabone72@aol.com>
2025-09-08 21:11:50 +0100

    deleteTransaction - 4

--- data/deleteTransaction.ts
+++ data/deleteTransaction.ts
@@ -1,5 +1,8 @@
 import authMiddleware from '@/authMiddleware'
+import { db } from '@/db'
+import { transactionsTable } from '@/db/schema'
 import { createServerFn } from '@tanstack/start'
+import { and, eq } from 'drizzle-orm'
 import { z } from 'zod'
 
 const schema = z.object({
@@ -7,4 +10,16 @@ const schema = z.object({
 
 export const deleteTransaction = createServerFn({
   method: 'POST'
-}).middleware([authMiddleware]).validator((data: z.infer<typeof schema>) => schema.parse(data))
+})
+  .middleware([authMiddleware])
+  .validator((data: z.infer<typeof schema>) => schema.parse(data))
+  .handler(async ({context, data}) => {
+    await db
+      .delete(transactionsTable)
+      .where(
+        and (
+          eq(transactionsTable.id, data.transactionId),
+          eq(transactionsTable.userId, context.userId)
+        )
+      )
+  })
