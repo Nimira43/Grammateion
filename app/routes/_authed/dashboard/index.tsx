0b49de9fedb577800739246ab9ed2f977d1253d6
Nimira43 <n.rabone72@aol.com>
2025-10-06 15:54:13 +0100

    Brought RecentTransactions into index.tsx

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { RecentTransactions } from './-recent-transactions'
 
 export const Route = createFileRoute('/_authed/dashboard/')({
   component: RouteComponent,
@@ -7,6 +8,6 @@ export const Route = createFileRoute('/_authed/dashboard/')({
 function RouteComponent() {
   return <div className='max-w-screen-xl mx-auto py-5'>
     <h1 className='text-4xl foont-medium pb-5'>Dashboard</h1>
-    
+    <RecentTransactions />
   </div>
 }
