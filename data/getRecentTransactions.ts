cf613abb88f3f3a5b2c645f045d38d1f61455996
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:00:15 +0100

    getRecentTransactions - 2

--- data/getRecentTransactions.ts
+++ data/getRecentTransactions.ts
@@ -1,3 +1,6 @@
+import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
 
-export const getRecentTransactions = createServerFn
+export const getRecentTransactions = createServerFn({
+  method: 'GET'
+}).middleware([authMiddleware])
