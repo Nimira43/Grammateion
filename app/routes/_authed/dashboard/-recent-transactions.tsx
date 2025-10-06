e44a85fa63590f19cef26959bc6cea49ea7d5041
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:44:26 +0100

    Setting up params and type for RecentTransactions

--- app/routes/_authed/dashboard/-recent-transactions.tsx
+++ app/routes/_authed/dashboard/-recent-transactions.tsx
@@ -1,6 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-export function RecentTransactions() {
+export function RecentTransactions({
+  transactions
+}: {
+  transactions: {
+    id: number
+    description: string
+    amount: string
+    category: string | null
+    transactionType: 'income' | 'expense' | null
+    transactionDate: string
+  }[]
+}) {
   return (
     <Card>
       <CardHeader>
