be7e6d3c2dc068032b704e8a34a8ddd9e3f38a72
Nimira43 <n.rabone72@aol.com>
2025-10-06 15:57:54 +0100

    Created getRecentTransactions.ts in data

--- app/routes/_authed/dashboard/-recent-transactions.tsx
+++ app/routes/_authed/dashboard/-recent-transactions.tsx
@@ -1,13 +1,14 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function RecentTransactions() {
   return (
     <Card>
       <CardHeader>
         <CardTitle>
-
+          Recent Transactions
         </CardTitle>
       </CardHeader>
+      <CardContent>Transactions</CardContent>
     </Card>
   )
 }
