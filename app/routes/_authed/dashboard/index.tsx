5567c9427d3b428164563b75e36a3b75add367f3
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:41:27 +0100

    Passing in transactions into RecentTransactions instance

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -17,6 +17,8 @@ function RouteComponent() {
 
   return <div className='max-w-screen-xl mx-auto py-5'>
     <h1 className='text-4xl foont-medium pb-5'>Dashboard</h1>
-    <RecentTransactions />
+    <RecentTransactions 
+      transactions={transactions}
+    />
   </div>
 }
