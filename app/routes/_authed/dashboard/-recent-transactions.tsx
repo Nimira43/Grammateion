7c164bdb4b320b7f1f2040f112a0c42aa312ac69
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:48:10 +0100

    Copied markup for the table in AllTransactions over to RecentTransactions

--- app/routes/_authed/dashboard/-recent-transactions.tsx
+++ app/routes/_authed/dashboard/-recent-transactions.tsx
@@ -1,4 +1,9 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table'
+import { format } from 'date-fns'
+import { Table, Badge, Link, LucidePencil } from 'lucide-react'
+import numeral from 'numeral'
+import { Button } from 'react-day-picker'
 
 export function RecentTransactions({
   transactions
@@ -19,7 +24,77 @@ export function RecentTransactions({
           Recent Transactions
         </CardTitle>
       </CardHeader>
-      <CardContent>Transactions</CardContent>
+      <CardContent>
+        {!!transactions.length && (
+          <Table className='mt-4'>
+            <TableHeader>
+              <TableRow>
+                <TableHead>
+                  Date
+                </TableHead>
+                <TableHead>
+                  Description
+                </TableHead>
+                <TableHead>
+                  Type
+                </TableHead>
+                <TableHead>
+                  Category
+                </TableHead>
+                <TableHead>
+                  Amount
+                </TableHead>
+                <TableHead />
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {transactions.map(transaction => (
+                <TableRow key={transaction.id}>
+                  <TableCell>
+                    {format(transaction.transactionDate, "do MMM yyyy")}
+                  </TableCell>
+                  <TableCell>
+                    {transaction.description}
+                  </TableCell>
+                  <TableCell className='uppercase'>
+                    <Badge className={
+                      transaction.transactionType === 'income'
+                      ? 'bg-green-400'
+                      : 'bg-red-400'
+                    }>
+                      {transaction.transactionType}
+                    </Badge>
+                  </TableCell>
+                  <TableCell>
+                    {transaction.category}
+                  </TableCell>
+                  <TableCell>
+                    £{numeral(transaction.amount).format('0,0[.]00')}
+                  </TableCell>
+                  <TableCell className='text-right'>
+                    <Button asChild>
+                      <Link
+                        onClick={() => {
+                          router.clearCache({
+                            filter: (router) => router.pathname !== `/dashboard/transactions/${transaction.id}`
+                          })
+                        }}
+                        to='/dashboard/transactions/$transactionid'
+                        params={{ transactionid: transaction.id.toString() }}               
+                      >
+                        <LucidePencil 
+                        size='icon' 
+                        aria-label='Edit Transactions'
+                      />
+                      </Link>
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </CardContent>
     </Card>
   )
 }
