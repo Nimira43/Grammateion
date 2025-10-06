33df0bc40aa1430cc33ed51f9608d8eb0c4c731c
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:15:15 +0100

    getRecentTransactions - 4

--- data/getRecentTransactions.ts
+++ data/getRecentTransactions.ts
@@ -2,18 +2,22 @@ import authMiddleware from '@/authMiddleware'
 import { db } from '@/db'
 import { transactionsTable, categoriesTable } from '@/db/schema'
 import { createServerFn } from '@tanstack/start'
+import { eq } from 'drizzle-orm'
 
 export const getRecentTransactions = createServerFn({
   method: 'GET'
 })
   .middleware([authMiddleware])
   .handler(async ({context}) => {
-    const transactions = await db.select({
-      id: transactionsTable.id,
-      description: transactionsTable.description,
-      amount: transactionsTable.amount,
-      transactionDate: transactionsTable.transactionDate,
-      category: categoriesTable.name,
-      transactionType: categoriesTable.type
-    })
+    const transactions = await db
+      .select({
+        id: transactionsTable.id,
+        description: transactionsTable.description,
+        amount: transactionsTable.amount,
+        transactionDate: transactionsTable.transactionDate,
+        category: categoriesTable.name,
+        transactionType: categoriesTable.type
+      })
+      .from(transactionsTable)
+      .where(eq(transactionsTable.userId, context.userId))
   })
