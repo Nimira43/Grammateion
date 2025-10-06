a923e78921e6e7b65af85e9bebecd4f5b88bc014
Nimira43 <n.rabone72@aol.com>
2025-10-06 16:40:03 +0100

    Desctructuring transactions

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -13,6 +13,8 @@ export const Route = createFileRoute('/_authed/dashboard/')({
 })
 
 function RouteComponent() {
+  const {transactions} = Route.useLoaderData()
+
   return <div className='max-w-screen-xl mx-auto py-5'>
     <h1 className='text-4xl foont-medium pb-5'>Dashboard</h1>
     <RecentTransactions />
