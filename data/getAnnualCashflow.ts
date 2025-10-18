956988760d017d02588fa3932a6e636667d3da3d
Nimira43 <n.rabone72@aol.com>
2025-10-18 15:37:13 +0100

    getAnnualCashflow - 3

--- data/getAnnualCashflow.ts
+++ data/getAnnualCashflow.ts
@@ -1,5 +1,8 @@
 import authMiddleware from '@/authMiddleware'
+import { db } from '@/db'
+import { categoriesTable, transactionsTable } from '@/db/schema'
 import { createServerFn } from '@tanstack/start'
+import { sql } from 'drizzle-orm'
 import { z } from 'zod'
 
 const schema = z.object({
@@ -8,4 +11,12 @@ const schema = z.object({
 
 export const getAnnualCashflow = createServerFn({
   method: 'GET'
-}).middleware([authMiddleware]).validator
+})
+  .middleware([authMiddleware])
+  .validator((data: z.infer<typeof schema>) =>schema.parse(data))
+  .handler(async () => {
+    const cashflow = await db.select({
+      month: sql<string>`EXTRACT(MONTH FROM ${transactionsTable.transactionDate})`,
+      totalIncome: sql<string>`SUM(CASE WHEN ${categoriesTable.type} = 'income' THEN ${transactionsTable.amount} ELSE 0 END)`,
+    })
+  })
