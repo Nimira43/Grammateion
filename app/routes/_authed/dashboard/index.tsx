7f237ce11ba678e961182c0ea6ee3be55d95fc12
Nimira43 <n.rabone72@aol.com>
2025-10-18 16:45:47 +0100

    Brought getAnnualCashflow into dashboard index.js - rendering cashflow total in the console.logs successfully.

--- app/routes/_authed/dashboard/index.tsx
+++ app/routes/_authed/dashboard/index.tsx
@@ -1,20 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { RecentTransactions } from './-recent-transactions'
 import { getRecentTransactions } from '@/data/getRecentTransactions'
+import { getAnnualCashflow } from '@/data/getAnnualCashflow'
 
 export const Route = createFileRoute('/_authed/dashboard/')({
   component: RouteComponent,
   loader: async () => {
-    const transactions = await getRecentTransactions()
+    const [transactions, cashflow] = await Promise.all([
+      getRecentTransactions(),  
+      getAnnualCashflow({
+        data: {
+          year: 2025
+        }
+      })
+    ])
+    console.log(cashflow)
     return {
+      cashflow,
       transactions,
     }
   }
 })
 
 function RouteComponent() {
-  const {transactions} = Route.useLoaderData()
-
+  const {transactions, cashflow} = Route.useLoaderData()
+  console.log(cashflow)
   return <div className='max-w-screen-xl mx-auto py-5'>
     <h1 className='text-4xl font-medium pb-5'>Dashboard</h1>
     <RecentTransactions 
