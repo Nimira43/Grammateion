f4f054edce821d6d110706d828c84320dc0ec55c
Nimira43 <n.rabone72@aol.com>
2025-10-18 14:52:40 +0100

    getAnnualCashflow - 2

--- data/getAnnualCashflow.ts
+++ data/getAnnualCashflow.ts
@@ -1,5 +1,10 @@
 import authMiddleware from '@/authMiddleware'
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
+
+const schema = z.object({
+  year: z.number()
+})
 
 export const getAnnualCashflow = createServerFn({
   method: 'GET'
