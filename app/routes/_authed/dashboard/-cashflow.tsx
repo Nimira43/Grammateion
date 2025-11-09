10515b7e979b6084d8a054a1dc4314e8bc410d33
Nimira43 <n.rabone72@aol.com>
2025-11-09 20:15:45 +0000

    Cashflow - 1

---
+++ app/routes/_authed/dashboard/-cashflow.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+
+export function Cashflow() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className='flex justify-between'>
+          <span>Cashflow</span>
+        </CardTitle>
+      </CardHeader>
+    </Card>
+  )
+}
