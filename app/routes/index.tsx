1967f371eb4244b899c78e5793e858bb80d57b70
Nimira43 <n.rabone72@aol.com>
2025-12-28 20:07:33 +0000

    Logo and text placeholders

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import cover from '@/assets/home-page.jpg'
+import { BookOpenCheck } from 'lucide-react'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -14,7 +15,16 @@ function RouteComponent() {
         className='absolute top-0 left-0 object-cover object-center h-full w-full'
       />
       <div className="absolute inset-0 bg-black/70" />
-      
+      <div className='flex flex-col text-center'>
+        <BookOpenCheck />
+        <h1 className='logo pl-2'>Grammateion</h1>
+      </div>
+      <div>
+        Site Description
+      </div>
+      <div>
+        Sign In | Sign Out Buttons
+      </div>
     </div>
   )
 }
